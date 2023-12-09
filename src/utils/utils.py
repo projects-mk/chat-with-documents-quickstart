887596b760d87e8bdd6ca223d51d1d219966e217
Mateusz <mateuszkurdziel1@gmail.com>
2023-12-09 02:01:20 +0100

    update chat

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -7,13 +7,17 @@ import streamlit as st
 import kubernetes
 import psycopg2
 from psycopg2.errors import OperationalError
+from dotenv import load_dotenv
+
+load_dotenv('../.env')
 
 
 def clear_cache():
     st.legacy_caching.caching.clear_cache()
 
-
 # flake8: noqa: E501
+
+
 def show_pdf_uplaoded(bytes_data):
     base64_pdf = base64.b64encode(bytes_data).decode('utf-8')
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="100" type="application/pdf"></iframe>'
@@ -32,28 +36,10 @@ class CheckResources:
             return 'Unavailable'
 
     @staticmethod
-    @st.cache_data
-    def check_cluster():
+    def check_llm():
         try:
-            kubernetes.config.load_kube_config()
-            api = kubernetes.client.CoreV1Api()
-            api.list_namespaced_pod(namespace='default')
-            return 'Running'
-        except kubernetes.config.config_exception.ConfigException:
-            return 'Unavailable'
-
-    @staticmethod
-    @st.cache_data
-    def check_database():
-        try:
-            response = psycopg2.connect(
-                host=os.getenv('DATABASE_HOST'),
-                port='5432',
-                password=os.getenv('POSTGRES_PASSWORD'),
-                user=os.getenv('POSTGRES_USER'),
-                database='postgres',
-            )
-            if response.code:
+            response = requests.get(os.getenv('LLM_HOST'))
+            if response.status_code == 200:
                 return 'Running'
-        except OperationalError:
+        except requests.exceptions.ConnectionError:
             return 'Unavailable'
