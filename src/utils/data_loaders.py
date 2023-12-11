6241425ca3f765f105557a14bf3a24a958478832
Mateusz <mateuszkurdziel1@gmail.com>
2023-12-11 22:37:16 +0100

    refactored some code

--- src/utils/data_loaders.py
+++ src/utils/data_loaders.py
@@ -5,8 +5,21 @@ from PyPDF2 import PdfReader
 
 
 class DocumentLoader:
+    """
+    This class provides methods to load documents from PDF files.
+    """
+
     @staticmethod
     def _read_pages(pdf_reader):
+        """
+        Reads the text from all pages of a PDF file.
+
+        Args:
+            pdf_reader (PdfReader): The PDF reader object.
+
+        Returns:
+            str: The text extracted from the PDF.
+        """
         pdf_content = ''
 
         for page in pdf_reader.pages:
@@ -16,6 +29,15 @@ class DocumentLoader:
 
     @staticmethod
     def read_pdf_from_bytes(bytes_data):
+        """
+        Reads a PDF file from bytes data.
+
+        Args:
+            bytes_data (bytes): The bytes data of the PDF file.
+
+        Returns:
+            str: The text extracted from the PDF.
+        """
         with io.BytesIO(bytes_data) as base64_pdf:
             reader = PdfReader(base64_pdf)
             doc = DocumentLoader._read_pages(reader)
@@ -24,22 +46,58 @@ class DocumentLoader:
 
     @staticmethod
     def read_pdf_from_path(filepath, pdf_password=None):
+        """
+        Reads a PDF file from a file path.
+
+        Args:
+            filepath (str): The path to the PDF file.
+            pdf_password (str, optional): The password for the PDF file. Defaults to None.
+
+        Returns:
+            str: The text extracted from the PDF.
+        """
         reader = PyPDFLoader(filepath, password=pdf_password)
 
         return reader.load()
 
 
 class StylesLoader:
+    """
+    This class provides methods to load CSS styles from a file.
+    """
+
     @staticmethod
     def load(css_file_path):
+        """
+        Loads CSS styles from a file.
+
+        Args:
+            css_file_path (str): The path to the CSS file.
+
+        Returns:
+            str: The CSS styles.
+        """
         with open(css_file_path, 'r', encoding='utf-8') as f:
             css = f.read()
         return f'<style>\n\n{css}\n</style>'
 
 
 class HtmlLoader:
+    """
+    This class provides methods to load HTML from a file.
+    """
+
     @staticmethod
     def load(html_file_path):
+        """
+        Loads HTML from a file.
+
+        Args:
+            html_file_path (str): The path to the HTML file.
+
+        Returns:
+            str: The HTML content.
+        """
         with open(html_file_path, 'r', encoding='utf-8') as f:
             html = f.read()
         return html
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -13,21 +13,41 @@ load_dotenv('../.env')
 
 
 def clear_cache():
+    """
+    Clears the cache in the Streamlit app.
+    """
     st.legacy_caching.caching.clear_cache()
-
-# flake8: noqa: E501
 
 
 def show_pdf_uplaoded(bytes_data):
+    """
+    Displays the uploaded PDF in the Streamlit app.
+
+    Args:
+        bytes_data (bytes): The binary data of the PDF.
+    """
     base64_pdf = base64.b64encode(bytes_data).decode('utf-8')
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="100" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 
 class CheckResources:
+    """
+    This class provides methods to check the status of various resources.
+    """
+
     @staticmethod
     @st.cache_data
     def check_service(service_url):
+        """
+        Checks the status of a service at the given URL.
+
+        Args:
+            service_url (str): The URL of the service to check.
+
+        Returns:
+            str: 'Running' if the service is available, 'Unavailable' otherwise.
+        """
         try:
             response = requests.get(service_url)
             if response.status_code == 200:
@@ -37,14 +57,35 @@ class CheckResources:
 
     @staticmethod
     def check_qdrant():
+        """
+        Checks the status of the Qdrant service.
+
+        Returns:
+            str: 'Running' if the service is available, 'Unavailable' otherwise.
+        """
         return CheckResources.check_service(os.getenv('QDRANT_HOST'))
 
     @staticmethod
     def check_llm():
+        """
+        Checks the status of the LLM service.
+
+        Returns:
+            str: 'Running' if the service is available, 'Unavailable' otherwise.
+        """
         return CheckResources.check_service(os.getenv('LLM_HOST'))
 
     @staticmethod
     def create_and_check_db_conn(conn_string):
+        """
+        Creates a database connection and checks its status.
+
+        Args:
+            conn_string (str): The connection string for the database.
+
+        Returns:
+            str: 'Running' if the connection is successful, 'Unavailable' otherwise.
+        """
         try:
             engine = create_engine(conn_string)
             engine.connect()
@@ -54,10 +95,22 @@ class CheckResources:
 
     @staticmethod
     def check_db():
+        """
+        Checks the status of the database connection.
+
+        Returns:
+            str: 'Running' if the connection is successful, 'Unavailable' otherwise.
+        """
         return CheckResources.create_and_check_db_conn(os.getenv('DATABASE_CONN_STRING'))
 
     @staticmethod
     def check_monitoring():
+        """
+        Checks the status of the monitoring service.
+
+        Returns:
+            str: 'Running' if the service is available, 'Unavailable' otherwise.
+        """
         conn_string = os.getenv('DATABASE_CONN_STRING')
         engine = create_engine(conn_string)
         df = pd.read_sql_table('monitoring', engine)
